bef0e816514cdb5066633120bc97ef9ddf47f589
Samira <s.badamestani@gmail.com>
2022-02-07 20:28:13 +0100

    added DifficultyEntity and dao

--- app/src/main/kotlin/com/sami/samfit/db/SamfitDatabase.kt
+++ app/src/main/kotlin/com/sami/samfit/db/SamfitDatabase.kt
@@ -2,10 +2,13 @@ package com.sami.samfit.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.sami.samfit.db.difficultySelection.DifficultyEntity
+import com.sami.samfit.db.difficultySelection.DifficultySelectionDao
 import com.sami.samfit.db.genderselection.GenderEntity
 import com.sami.samfit.db.genderselection.GenderSelectionDao
 
-@Database(entities = [GenderEntity::class], version = DatabaseVersion.VERSION_1)
+@Database(entities = [GenderEntity::class, DifficultyEntity::class], version = DatabaseVersion.VERSION_1)
 abstract class SamfitDatabase : RoomDatabase() {
     abstract fun genderSelectionDao(): GenderSelectionDao
+    abstract fun difficultyLevelDao(): DifficultySelectionDao
 }
